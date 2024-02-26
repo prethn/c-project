84344665c6d0e93d2bd25627ddc51c3e24b8af08
prethn <3093563482@qq.com>
2024-02-26 18:33:49 +0800

    add tarball again

--- demo1/hello.cpp
+++ demo1/hello.cpp
@@ -5,13 +5,9 @@ int main()
     int var_a;
     cout << "请输入： ";
     cin >>var_a;
-    cout << "come up and c++ me some time.1";cout << "come up and 
-    c++ me some time.";
+    cout << "come up and c++ me some time.1";cout << "come up and c++ me some time.";
     // printf("\n");
     cout <<endl;cout << "You won't regret it!" << endl;
-    cout << "the value of var_a : " <<
-     var_a
-      <<
-       endl;
+    cout << "the value of var_a : " <<var_a<<endl;
     return 0;
 }
