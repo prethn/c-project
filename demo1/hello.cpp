baa59cb42a8c9e4a89ba1a61b9f554b8552514da
prethn <3093563482@qq.com>
2024-02-26 19:21:39 +0800

    add

--- demo1/hello.cpp
+++ demo1/hello.cpp
@@ -5,9 +5,11 @@ int main()
     int var_a;
     cout << "请输入： ";
     cin >>var_a;
-    cout << "come up and c++ me some time.1";cout << "come up and c++ me some time.";
+    cout << "come up and c++ me some time.1";
+    cout << "come up and c++ me some time.";
     // printf("\n");
     cout <<endl;cout << "You won't regret it!" << endl;
     cout << "the value of var_a : " <<var_a<<endl;
+    cout << var_a << endl;
     return 0; 
 }
