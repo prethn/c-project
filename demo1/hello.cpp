c8546daa6e9b516ec08eb409614603d772e3316b
prethn <3093563482@qq.com>
2024-02-26 20:34:26 +0800

    ch2 learnt

--- demo1/hello.cpp
+++ demo1/hello.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+// prototype of function
+void statement_u2_3();
+void sprt_u2_4();
+
 int main()
+{
+    // statement_u2_3();
+    sprt_u2_4();
+    return 0; 
+}
+
+void statement_u2_3()
 {
     int var_a;
     cout << "请输入： ";
@@ -11,5 +24,15 @@ int main()
     cout <<endl;cout << "You won't regret it!" << endl;
     cout << "the value of var_a : " <<var_a<<endl;
     cout << var_a << endl;
-    return 0; 
+}
+void sprt_u2_4()
+{
+    double area; 
+    cout << "Enter the floor area, in square feet, of your home: ";
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of a square " << side
+         << " feet to the side" << endl;
+    cout << "How fascinating" << endl;
 }
