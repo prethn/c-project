305bf62044216e12bf4329ff3dfac71f168052eb
prethn <3093563482@qq.com>
2024-02-27 21:53:02 +0800

    add

--- ch4/ch4.cpp
+++ ch4/ch4.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 
 void string4_2();
-
+void (* func)();
+void (* fun[])() = {str_obj, string4_2, address_input};//函数表
 int main()
 {
-    int sum;
+    int sum, i;
     sum = add(8,5);
     cout << "sum = " << sum << endl; 
     // string4_2();
     // strings();
     // input_str();
     // address_input();
-    str_obj();
+
+    // func = str_obj;
+    // func();
+    
+    // for(i=1; i<3; i++)
+    // {
+    //     (*fun[i])();
+    //     cin.get();
+    // }
+    
 }
 
 void string4_2()
@@ -24,5 +34,5 @@ void string4_2()
     // cout << dog << endl;
     cout << cat << endl;
     cout << "The entire high-level implementation of the model " 
-            "is contained in whisper.h and whisper.cpp.";
+            "is contained in whisper.h and whisper.cpp." << endl; 
 }
